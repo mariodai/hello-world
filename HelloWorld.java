6cb97fa1a058f16f117b6f73d60449a4752228c1
mariodai <daizhenghw@outlook.com>
2022-07-14 22:16:33 +0800

    test

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld{
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("testxx");
         System.out.println("test2");
     }
 }
